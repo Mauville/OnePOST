12c7292e0bed6115e97bce84d6e6d0c4f43bcdee
Mauville <4991127@outlook.com>
2021-09-17 18:32:52 -0500

    Enable posting with Twitter API

--- app/Facades/SocialPoster/SocialBackends/TwitterBackend.php
+++ app/Facades/SocialPoster/SocialBackends/TwitterBackend.php
@@ -2,19 +2,33 @@
 
 namespace App\Facades\SocialPoster\SocialBackends;
 
+use Abraham\TwitterOAuth\TwitterOAuth;
 use App\Models\Artwork;
+use Illuminate\Support\Facades\Log;
 
 class TwitterBackend implements SocialBackend
 {
     private $token;
 
-    public function __construct($token)
+    public function __construct($token, $secrettoken)
     {
         $this->token = $token;
+        $this->secret_token = $secrettoken;
     }
 
-    public function post(Artwork $artwork): string
+    public function post(Artwork $artwork)
     {
+        Log::info("Begin twitterPOST");
+        $connection = new TwitterOAuth(config("twitter.consumer_key"), config("twitter.consumer_secret"), $this->token, $this->secret_token);
+        $connection->setTimeouts(10, 15);
+        $media = $connection->upload('media/upload', ['media' => storage_path("app/" . $artwork->URI)]);
+        $parameters = [
+            'status' => $artwork->description,
+//            'media_ids' => implode(',', [$media->media_id_string])
+            'media_ids' => $media->media_id_string
+        ];
+        $result = $connection->post('statuses/update', $parameters);
+        return $result;
         // TODO: Implement post() method.
     }
 
